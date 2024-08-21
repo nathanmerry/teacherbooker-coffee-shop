e9e847e1ba61bcf21c7cb3940bd4d2f00c72264f
nathanmerry <46092065+nathanmerry@users.noreply.github.com>
2024-08-21 19:00:06 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,18 +4,19 @@ import heroImage from './assets/images/hero.png';
 const App = () => {
   return (
     <div className="poppins">
-      <header></header>
-      <section className="border-t border-b pb-10 pt-10">
-        <div className="flex container mx-auto">
-          <img src={heroImage} alt="" className="rounded shadow h-[180px]" />
-          <div className="ml-8">
-            <h1 className="mb-3 text-2xl font-bold">Teacher Booker Coffee Shop</h1>
-            <div>
-              Indulge in a personalized coffee experience like never before. Choose from our curated selection of premium coffee blends or craft your own unique brew with our easy-to-use customization options. Whether you're a latte lover, an espresso enthusiast, or craving something sweet and creamy, Coffee Corner lets you order your perfect cup just the way you like it—delivered fresh and fast. Start your day right with the coffee that’s made for you, by you.
+      <header>
+        <section className="border-t border-b pb-10 pt-10">
+          <div className="flex container mx-auto">
+            <img src={heroImage} alt="" className="rounded shadow h-[180px]" />
+            <div className="ml-8">
+              <h1 className="mb-3 text-2xl font-bold">Teacher Booker Coffee Shop</h1>
+              <div>
+                Indulge in a personalized coffee experience like never before. Choose from our curated selection of premium coffee blends or craft your own unique brew with our easy-to-use customization options. Whether you're a latte lover, an espresso enthusiast, or craving something sweet and creamy, Coffee Corner lets you order your perfect cup just the way you like it—delivered fresh and fast. Start your day right with the coffee that’s made for you, by you.
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </header>
       <main className="bg-gray-100 shadow-inner">
         <Shop />
       </main>
